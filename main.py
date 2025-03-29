882efca79567ee3ecd2f43eb2888aa460af07438
Kaisar-salam <kaisar.torebek@mail.ru>
2025-03-29 12:15:49 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import telebot 
+import os
+import random
+import requests
+images = (os.listdir('photo'))
+
+
+bot=telebot.TeleBot("7712211031:AAG99gaWC85O-GU1OzdnRC5ed8VRsEBL3sw")
+
+@bot.message_handler(commands=['mem'])
+def send_mem(message):
+    image = random.choice(images)
+    with open(f'photo/{image}', 'rb') as f:  
+        bot.send_photo(message.chat.id, f)  
+def get_duck_image_url():    
+        url = 'https://random-d.uk/api/random'
+        res = requests.get(url)
+        data = res.json()
+        return data['url']
+@bot.message_handler(commands=['duck'])
+def duck(message):
+        '''По команде duck вызывает функцию get_duck_image_url и отправляет URL изображения утки'''
+        image_url = get_duck_image_url()
+        bot.reply_to(message, image_url)
+bot.polling()
